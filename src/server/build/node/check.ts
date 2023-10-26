5a6dc4c90cb8d3de1b6a445bd9acd82ccae8407c
Chris Pirich <5429146+cpirich@users.noreply.github.com>
2023-10-26 11:50:11 -0700

    exec with fake nextjs env variables

--- src/server/build/node/check.ts
+++ src/server/build/node/check.ts
@@ -2,12 +2,38 @@ import { execAsyncWithLog } from "../../utils";
 
 type ExecPromise = ReturnType<typeof execAsyncWithLog>;
 
+const NEXT_JS_ENV = {
+  NEXTAUTH_SECRET: "NEXTAUTH_SECRET",
+  GITHUB_ID: "GITHUB_ID",
+  GITHUB_SECRET: "GITHUB_SECRET",
+  EMAIL_FROM: "EMAIL_FROM",
+  EMAIL_SERVER_HOST: "EMAIL_SERVER_HOST",
+  EMAIL_SERVER_PORT: "EMAIL_SERVER_PORT",
+  EMAIL_SERVER_USER: "EMAIL_SERVER_USER",
+  EMAIL_SERVER_PASSWORD: "EMAIL_SERVER_PASSWORD",
+};
+
 async function executeWithLogRequiringSuccess(
   path: string,
   command: string,
 ): ExecPromise {
   console.log(`*:${command} (cwd: ${path})`);
-  const result = await execAsyncWithLog(command, { cwd: path });
+  const {
+    GITHUB_PRIVATE_KEY, // eslint-disable-line @typescript-eslint/no-unused-vars
+    GITHUB_APP_ID, // eslint-disable-line @typescript-eslint/no-unused-vars
+    GITHUB_CLIENT_ID, // eslint-disable-line @typescript-eslint/no-unused-vars
+    GITHUB_CLIENT_SECRET, // eslint-disable-line @typescript-eslint/no-unused-vars
+    GITHUB_WEBHOOK_SECRET, // eslint-disable-line @typescript-eslint/no-unused-vars
+    OPENAI_API_KEY, // eslint-disable-line @typescript-eslint/no-unused-vars
+    DATABASE_URL, // eslint-disable-line @typescript-eslint/no-unused-vars
+    VITE_GITHUB_CLIENT_ID, // eslint-disable-line @typescript-eslint/no-unused-vars
+    VITE_FIGMA_PLUGIN_ID, // eslint-disable-line @typescript-eslint/no-unused-vars
+    ...baseEnv
+  } = process.env;
+  const result = await execAsyncWithLog(command, {
+    cwd: path,
+    env: { ...baseEnv, ...NEXT_JS_ENV },
+  });
 
   if (result.exitCode !== 0) {
     throw new Error(`${command} failed with exit code: ${result.exitCode}`);
@@ -17,12 +43,6 @@ async function executeWithLogRequiringSuccess(
 }
 
 export async function runBuildCheck(path: string): ExecPromise {
-  console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-  try {
-    await executeWithLogRequiringSuccess(path, "asdf --version");
-  } catch (error) {
-    console.error(error);
-  }
   await executeWithLogRequiringSuccess(path, "node --version");
   await executeWithLogRequiringSuccess(path, "npm --version");
   await executeWithLogRequiringSuccess(path, "npm install");
