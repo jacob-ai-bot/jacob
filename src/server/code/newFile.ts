c3a636871bfedaf3ebed5c5162b7f4195f908be0
kleneway <kleneway@gmail.com>
2024-01-18 14:20:25 -0800

    added portkey, added gpt-4-turbo + gpt-4-vision endpoints, updated default model to turbo

--- src/server/code/newFile.ts
+++ src/server/code/newFile.ts
@@ -18,7 +18,7 @@ export async function createNewFile(
   token: string,
   issue: Issue,
   rootPath: string,
-  repoSettings?: RepoSettings
+  repoSettings?: RepoSettings,
 ) {
   const planTemplateParams = {
     newFileName,
@@ -29,18 +29,18 @@ export async function createNewFile(
     "dev",
     "plan_new_file",
     "system",
-    planTemplateParams
+    planTemplateParams,
   );
   const planUserPrompt = parseTemplate(
     "dev",
     "plan_new_file",
     "user",
-    planTemplateParams
+    planTemplateParams,
   );
   const plan = (await sendGptRequest(
     planUserPrompt,
     planSystemPrompt,
-    0.2
+    0.2,
   )) as string;
 
   const sourceMap = getSourceMap(rootPath, repoSettings);
@@ -58,18 +58,18 @@ export async function createNewFile(
   const codeSystemPrompt = constructNewOrEditSystemPrompt(
     "code_new_file",
     codeTemplateParams,
-    repoSettings
+    repoSettings,
   );
   const codeUserPrompt = parseTemplate(
     "dev",
     "code_new_file",
     "user",
-    codeTemplateParams
+    codeTemplateParams,
   );
   const code = (await sendGptRequest(
     codeUserPrompt,
     codeSystemPrompt,
-    0.2
+    0.2,
   )) as string;
 
   if (code.length < 10) {
@@ -79,7 +79,7 @@ export async function createNewFile(
   }
 
   // if a line of the code starts with ``` then it is a code block delimiter from GPT. Remove these lines.
-  const realCode = code.replace(/^```.*$/gm, "");
+  const realCode = code.replace(/^```.*\n?/gm, "");
 
   const newBranch = `jacob-issue-${issue.number}-${Date.now()}`;
 
--- src/server/openai/request.ts
+++ src/server/openai/request.ts
@@ -10,6 +10,13 @@ import { parse } from "jsonc-parser";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
+  baseURL: "https://api.portkey.ai/v1/proxy",
+  defaultHeaders: {
+    "x-portkey-api-key": process.env.PORTKEY_API_KEY,
+    "x-portkey-mode": "proxy openai",
+    "x-portkey-cache": "simple",
+    "x-portkey-retry-count": "3",
+  },
 });
 
 const CONTEXT_WINDOW = {
@@ -29,7 +36,7 @@ type Model = keyof typeof CONTEXT_WINDOW;
 
 export const getMaxTokensForResponse = async (
   inputText: string,
-  model: Model
+  model: Model,
 ): Promise<number> => {
   try {
     const enc = encodingForModel(model);
@@ -44,7 +51,7 @@ export const getMaxTokensForResponse = async (
 
     if (maxTokensForResponse <= 0) {
       throw new Error(
-        "Input text is too large to fit within the context window."
+        "Input text is too large to fit within the context window.",
       );
     }
 
@@ -60,17 +67,17 @@ export const sendGptRequest = async (
   systemPrompt = "You are a helpful assistant.",
   temperature = 0.2,
   retries = 10,
-  delay = 60000 // rate limit is 40K tokens per minute, so by default start with 60 seconds
+  delay = 60000, // rate limit is 40K tokens per minute, so by default start with 60 seconds
 ) => {
   console.log("\n\n --- User Prompt --- \n\n", userPrompt);
   console.log("\n\n --- System Prompt --- \n\n", systemPrompt);
 
-  const model = "gpt-4-vision-preview";
+  const model = "gpt-4-1106-preview";
 
   try {
     const max_tokens = await getMaxTokensForResponse(
       userPrompt + systemPrompt,
-      model
+      model,
     );
 
     console.log(`\n +++ Calling ${model} with max_tokens: ${max_tokens} `);
@@ -79,25 +86,6 @@ export const sendGptRequest = async (
       model,
       messages: [
         { role: "system", content: systemPrompt },
-        {
-          role: "user",
-          content: [
-            {
-              type: "image_url",
-              image_url: {
-                url: "https://jacob-images-staging.s3.us-west-2.amazonaws.com/Product+Detail.png",
-                detail: "high",
-              },
-            },
-            {
-              type: "text",
-              text: `Act as an expert-level front-end software engineer and designer. The IMAGE above is EXACTLY the design that you MUST implement with code.
-            A low-level developer has provided some basic code to get you started. This code WILL have errors and you MUST use this design to correct for those errors. 
-            If you can not correct for the errors, you MUST use the design to write your own code. Your code MUST be pixel-perfect! If you can achieve this on the first try, you will get a $100,000 bonus.
-            You MUST use this code ONLY as a starting point, but you MUST follow all the instructions below.`,
-            },
-          ],
-        },
         { role: "user", content: userPrompt },
       ],
       max_tokens,
@@ -117,7 +105,7 @@ export const sendGptRequest = async (
       throw error;
     } else {
       console.log(
-        `Received 429, retries remaining: ${retries}. Retrying in ${delay} ms...`
+        `Received 429, retries remaining: ${retries}. Retrying in ${delay} ms...`,
       );
       return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -126,7 +114,7 @@ export const sendGptRequest = async (
             systemPrompt,
             temperature,
             retries - 1,
-            delay * 2
+            delay * 2,
           )
             .then(resolve)
             .catch(reject);
@@ -143,7 +131,7 @@ export const sendGptRequestWithSchema = async (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   zodSchema: ZodSchema<any>,
   maxRetries: number = 3,
-  temperature: number = 0.2
+  temperature: number = 0.2,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
 ): Promise<any> => {
   let extractedInfo;
@@ -156,47 +144,46 @@ export const sendGptRequestWithSchema = async (
     try {
       gptResponse = (await sendGptRequest(
         userPrompt,
-        systemPrompt +
-          "IMPORTANT! YOU MUST enclose ALL code in ``` TypeScript code blocks ```",
-        temperature // Use a lower temperature for retries
+        systemPrompt,
+        temperature, // Use a lower temperature for retries
       )) as string;
 
       if (!gptResponse) {
         throw new Error("/n/n/n/n **** Empty response from GPT **** /n/n/n/n");
       }
 
-      // Remove lines that start with ```
-      gptResponse = gptResponse.replace(/^```.*$/gm, "");
+      // GPT will often use ``` when outputting code. We need to remove any lines that start with ```
+      gptResponse = gptResponse.replace(/^```.*\n?/gm, "");
 
       extractedInfo = parse(gptResponse);
 
       // if the response is an array of objects, validate each object individually and return the full array if successful
       if (Array.isArray(extractedInfo)) {
         const validatedInfo = extractedInfo.map(
-          (info) => zodSchema.safeParse(info) // as SafeParseReturnType<any, any>,
+          (info) => zodSchema.safeParse(info), // as SafeParseReturnType<any, any>,
         );
 
         const failedValidations = validatedInfo.filter(
-          (result) => result.success === false
+          (result) => result.success === false,
         );
 
         if (failedValidations.length > 0) {
           throw new Error(
             `Invalid response from GPT - object is not able to be parsed using the provided schema: ${JSON.stringify(
-              failedValidations
-            )}`
+              failedValidations,
+            )}`,
           );
         }
 
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         return (validatedInfo as SafeParseSuccess<any>[]).map(
-          (result) => result.data
+          (result) => result.data,
         );
       }
 
       // if the response is a single object, validate it and return it if successful
       const validationResult = zodSchema.safeParse(
-        extractedInfo
+        extractedInfo,
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
       ) as SafeParseReturnType<any, any>;
 
@@ -207,13 +194,13 @@ export const sendGptRequestWithSchema = async (
       throw new Error(
         `Invalid response from GPT - object is not able to be parsed using the provided schema: ${JSON.stringify(
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          (validationResult as SafeParseError<any>).error
-        )}`
+          (validationResult as SafeParseError<any>).error,
+        )}`,
       );
     } catch (error) {
       console.log(
         `Error occurred during GPT request: ${(error as { message?: string })
-          ?.message}`
+          ?.message}`,
       );
       retries++;
     }
