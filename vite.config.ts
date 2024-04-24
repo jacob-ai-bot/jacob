9c6721354e0f2f8b64b28d261ae38ffece287f56
Chris Pirich <5429146+cpirich@users.noreply.github.com>
2024-04-24 10:49:16 -0700

    Remove old tsconfig and related files

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import express from "./express-plugin";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), express("src/server")],
-  clearScreen: false,
-});
