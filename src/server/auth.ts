a723dacc53598988f3456a02e13ea8ae3a378ecd
Chris Pirich <5429146+cpirich@users.noreply.github.com>
2024-05-07 14:32:50 -0700

    Add refreshGitHubAccessToken()

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -2,7 +2,9 @@ import {
   getServerSession,
   type DefaultSession,
   type NextAuthOptions,
+  type TokenSet,
 } from "next-auth";
+import type { JWT } from "next-auth/jwt";
 import GitHubProvider from "next-auth/providers/github";
 
 import { env } from "~/env";
@@ -29,6 +31,57 @@ declare module "next-auth" {
   // }
 }
 
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string;
+    refreshToken?: string;
+    accessTokenExpires?: Date;
+  }
+}
+
+async function refreshGitHubAccessToken(token: JWT) {
+  try {
+    const requestBody = {
+      client_id: env.GITHUB_CLIENT_ID,
+      client_secret: env.GITHUB_CLIENT_SECRET,
+      grant_type: "refresh_token",
+      refresh_token: token.refreshToken,
+    };
+
+    const response = await fetch(
+      "https://github.com/login/oauth/access_token",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(requestBody),
+      },
+    );
+
+    const refreshedTokens = (await response.json()) as TokenSet;
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: new Date(refreshedTokens.expires_at ?? 0 * 1000),
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
+
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
  *
@@ -50,8 +103,18 @@ export const authOptions: NextAuthOptions = {
       // Persist the OAuth access_token and or the user id to the token right after signin
       if (account) {
         token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.accessTokenExpires = new Date(account.expires_at ?? 0 * 1000);
+        return token;
       }
-      return token;
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < Number(token.accessTokenExpires)) {
+        return token;
+      }
+
+      // Access token has expired, try to update it
+      return refreshGitHubAccessToken(token);
     },
   },
   providers: [
