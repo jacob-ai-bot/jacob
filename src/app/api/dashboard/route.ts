bc6f1d82790276a85e154c0bd950907ab0a97605
Chris Pirich <5429146+cpirich@users.noreply.github.com>
2024-05-15 10:55:32 -0700

    Change from Request to NextRequest

--- src/app/api/dashboard/route.ts
+++ src/app/api/dashboard/route.ts
@@ -1,17 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const org = searchParams.get("org");
   const repo = searchParams.get("repo");
 
   if (org && repo) {
     const response = NextResponse.redirect(
-      new URL(`/dashboard/${org}/${repo}`, request.url),
+      new URL(`/dashboard/${org}/${repo}`, request.nextUrl),
     );
     response.cookies.set("lastUsedRepo", `${org}/${repo}`);
     return response;
   }
 
-  return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.redirect(new URL("/", request.nextUrl));
 }
