2b31a6dd2d08c1304a954dc2d5c312260c61d7ae
kleneway <kleneway@gmail.com>
2024-08-28 12:56:41 -0700

    fixed bug with vitest hanging

--- src/server/api/routers/onboarding.ts
+++ src/server/api/routers/onboarding.ts
@@ -2,9 +2,10 @@
 
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { runBuildCheck } from "~/server/build/node/check";
 import { db } from "~/server/db/db";
 import { cloneRepo } from "~/server/git/clone";
-import { generateRepoSettings } from "~/server/utils/settings";
+import { generateRepoSettings, getRepoSettings } from "~/server/utils/settings";
 
 // TODO: NAVIGATE TO A PAGE THAT FIRST CHECKS TO SEE IF IT'S A NODE PROJECT, THEN CHECKS TO SEE IF IT BUILDS. IF IT DOESN'T BUILD, THEN TAKE THE ERROR MESSAGES AND TRY TO USE IT AS A FIRST SAMPLE PROJECT. NAVIGATE TO THE NEW DASHBOARD THAT HAS THE CONTEXT VISUALIZER (SHOW THIS FIRST THEN THE LIVE CHAT THEN THE "TODOS" WITH THE MORE DETAILED ISSUES AND THEN THE "SHOW WHAT JACOB IS DOING" SECTION.
 export const onboardingRouter = createTRPCRouter({
@@ -56,4 +57,55 @@ export const onboardingRouter = createTRPCRouter({
       // Update the project with the new settings
       await db.projects.find(project.id).update({ settings });
     }),
+  checkBuild: protectedProcedure
+    .input(
+      z.object({
+        org: z.string(),
+        repoName: z.string(),
+      }),
+    )
+    .query(
+      async ({
+        input: { org, repoName },
+        ctx: {
+          session: { accessToken, user },
+        },
+      }) => {
+        let cleanupClone: (() => Promise<void>) | undefined;
+        let buildErrorMessage: string | undefined;
+        try {
+          const repoFullName = `${org}/${repoName}`;
+          const { path, cleanup } = await cloneRepo({
+            repoName: repoFullName,
+            token: accessToken,
+          });
+          cleanupClone = cleanup;
+          const repoSettings = await getRepoSettings(path, repoFullName);
+          const project = await db.projects.findBy({
+            repoFullName,
+          });
+          if (!project) {
+            throw new Error("Project not found");
+          }
+          try {
+            await runBuildCheck({
+              path,
+              afterModifications: false,
+              repoSettings,
+              projectId: project.id,
+              repoFullName: repoFullName,
+              userId: user.id,
+            });
+          } catch (error) {
+            const { message } = error as Error;
+            buildErrorMessage = message;
+          }
+        } finally {
+          if (cleanupClone) {
+            await cleanupClone();
+          }
+        }
+        return buildErrorMessage;
+      },
+    ),
 });
